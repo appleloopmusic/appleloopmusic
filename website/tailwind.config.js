86c754c13174c16728d11ee21e16773a6b71c925
WilliamMokoena <swmokoena412@gmail.com>
2021-10-10 09:46:59 +0000

    Fixed babel and tailwind issues breaking build

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: [
+    "./src/**/*.html",
+    "./src/**/*.jsx",
+    "./src/**/*.tsx",
+    "./src/**/*.md",
+    "./src/**/*.mdx",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
